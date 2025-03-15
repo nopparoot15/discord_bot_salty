f3b374f0780f2af06fd3add4b7e2694d548c0b94
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2025-03-15 12:32:53 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Server is running!"
+    return "Discord Bot is Running!"
 
 def run():
-    app,run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def server_on():
-    t = Thread(target=run)
-    t.start()
+    thread = Thread(target=app.run, kwargs={"host":"0.0.0.0","port":8080})
+    thread.start()
