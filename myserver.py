501083555c6dc1f632199fcc9e0d238a50510822
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2025-03-16 03:21:26 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,22 +1,15 @@
-import uvicorn
-from fastapi import FastAPI
-from log_utils import setup_logging, send_log_to_discord
+from flask import Flask
+from threading import Thread
 
-app = FastAPI()
-logger = setup_logging()
-webhook_url = "https://discord.com/api/webhooks/1350546611327078464/17AFMw_4NM7bvaArtO52Sl1CkThz9gJqai5V4CwJS2J0UD_H3up1nyDsheFSD93ODxbu"
+app = Flask('')
 
-@app.get("/")
-def read_root():
-    logger.info("Root endpoint was called")
-    send_log_to_discord(webhook_url, "Root endpoint was called")
-    return {"Hello": "World"}
+@app.route('/')
+def home():
+    return "Server is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
 def server_on():
-    try:
-        logger.info("Server is starting")
-        send_log_to_discord(webhook_url, "Server is starting")
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    except Exception as e:
-        logger.error("Error occurred while starting the server", exc_info=e)
-        send_log_to_discord(webhook_url, f"Error occurred while starting the server: {str(e)}")
+    t = Thread(target=run)
+    t.start()
