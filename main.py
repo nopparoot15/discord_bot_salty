da49009147b5f4bc9c02922add67dea29cb1480e
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2025-03-23 15:50:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import os
+import sys
+import time
+import asyncio
+import aiohttp
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ui import View, Button, Modal, TextInput, Select
+from math import ceil
 
 class AnonymousMessageModal(Modal):
     def __init__(self, user_id: int):
